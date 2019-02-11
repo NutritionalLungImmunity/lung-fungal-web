e6e5fc11c75be49ae7075627ca411f08e9b6e1c2
Brian Helba <brian.helba@kitware.com>
2019-02-11 13:34:29 -0500

    Fix some issues with the build process

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,35 @@
+const path = require('path');
+const process = require('process');
+
 module.exports = {
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.glsl/,
-          loader: 'shader-loader',
-        },
-      ],
-    },
+  chainWebpack: (config) => {
+    // Alias "@" to the "./src" directory for imports
+    config.resolve.alias
+      .set('@$', path.resolve(__dirname, 'src'));
+
+    // Add vtk.js shader loader
+    config.module
+      .rule('glsl')
+      .test(/\.glsl$/)
+      .use('shader-loader')
+      .loader('shader-loader');
+
+    // Add vtk.js worker loader
+    config.module
+      .rule('worker')
+      .test(/\.worker\.js$/)
+      .use('worker-loader')
+      .loader('worker-loader')
+      .options({
+        inline: true,
+        fallback: false,
+      });
+
+    // Fix an issue with HMR and the worker-loader
+    // https://github.com/webpack/webpack/issues/6642
+    // https://github.com/vuejs/vue-cli/issues/2276
+    if (process.env.NODE_ENV !== 'production') {
+      config.output.globalObject('this');
+    }
   },
 };
