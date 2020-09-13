5c843d8c6b64d7ae3117151177025f9f58cfebd1
Jared Tomeck <jared.tomeck@kitware.com>
2020-09-13 11:38:59 -0400

    Add navigation

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,18 @@
       <v-toolbar-title>
         FLungGui
       </v-toolbar-title>
+      <v-spacer />
+      <v-toolbar-items>
+        <v-tabs
+          background-color="transparent"
+          height="64"
+        >
+          <v-tab to="config">Configuration</v-tab>
+          <v-tab to="viewer">Viewer</v-tab>
+          <v-tab to="simulations">Simulation List</v-tab>
+          <v-tab to="compare">Compare</v-tab>
+        </v-tabs>
+      </v-toolbar-items>
     </v-app-bar>
 
     <v-main>
