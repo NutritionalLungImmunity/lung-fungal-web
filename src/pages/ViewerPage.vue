80df1f63e9f1884feae30fbfc9ba353f14da2d21
Jared Tomeck <jared.tomeck@kitware.com>
2020-09-16 09:56:15 -0400

    Create run button

--- src/pages/ViewerPage.vue
+++ src/pages/ViewerPage.vue
@@ -7,103 +7,123 @@
       dark
       permanent
     >
-      <v-card
-        color="transparent"
-        flat
-        tile
-      >
-        <v-card-subtitle
-          class="align-center d-flex grey--text py-2"
-        >
-          Viewer Controls
-          <v-spacer />
-          <div class="expansion-controls">
-            <v-tooltip top>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  v-if="panel.length < 3"
-                  icon
-                  small
-                  @click="all"
-                  v-on="on"
-                >
-                  <v-icon size="16">
-                    mdi-arrow-expand-vertical
-                  </v-icon>
-                </v-btn>
-                <v-btn
-                  v-else
-                  icon
-                  disabled
-                  small
-                >
-                  <v-icon size="16">
-                    mdi-arrow-expand-vertical
-                  </v-icon>
-                </v-btn>
-              </template>
-              <span>Expand All</span>
-            </v-tooltip>
-            <v-tooltip top>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  v-if="panel.length > 0"
-                  icon
-                  small
-                  @click="none"
-                  v-on="on"
-                >
-                  <v-icon size="16">
-                    mdi-arrow-collapse-vertical
-                  </v-icon>
-                </v-btn>
-                <v-btn
-                  v-else
-                  icon
-                  disabled
-                  small
-                >
-                  <v-icon size="16">
-                    mdi-arrow-collapse-vertical
-                  </v-icon>
-                </v-btn>
-              </template>
-              <span>Collapse All</span>
-            </v-tooltip>
-          </div>
-        </v-card-subtitle>
-        <v-divider />
-        <v-expansion-panels
-          v-model="panel"
-          accordion
+      <div class="drawer-wrapper d-flex flex-column">
+        <v-card
+          color="transparent"
           flat
-          multiple
           tile
         >
-          <v-expansion-panel
-            v-for="(item,i) in viewPanels"
-            :key="i"
-            class="grey darken-3"
+          <v-card-subtitle
+            class="align-center d-flex grey--text py-2"
           >
-            <v-expansion-panel-header>
-              {{ item.header }}
-            </v-expansion-panel-header>
-            <v-expansion-panel-content>
-              Controls here
-              <div class="d-flex panel-footer pt-3">
-                <v-spacer />
-                <v-btn
-                  color="primary"
-                  small
-                >
-                  Apply
-                </v-btn>
-              </div>
-            </v-expansion-panel-content>
-            <v-divider />
-          </v-expansion-panel>
-        </v-expansion-panels>
-      </v-card>
+            Viewer Controls
+            <v-spacer />
+            <div class="expansion-controls">
+              <v-tooltip top>
+                <template v-slot:activator="{ on }">
+                  <v-btn
+                    v-if="panel.length < 3"
+                    icon
+                    small
+                    @click="all"
+                    v-on="on"
+                  >
+                    <v-icon size="16">
+                      mdi-arrow-expand-vertical
+                    </v-icon>
+                  </v-btn>
+                  <v-btn
+                    v-else
+                    icon
+                    disabled
+                    small
+                  >
+                    <v-icon size="16">
+                      mdi-arrow-expand-vertical
+                    </v-icon>
+                  </v-btn>
+                </template>
+                <span>Expand All</span>
+              </v-tooltip>
+              <v-tooltip top>
+                <template v-slot:activator="{ on }">
+                  <v-btn
+                    v-if="panel.length > 0"
+                    icon
+                    small
+                    @click="none"
+                    v-on="on"
+                  >
+                    <v-icon size="16">
+                      mdi-arrow-collapse-vertical
+                    </v-icon>
+                  </v-btn>
+                  <v-btn
+                    v-else
+                    icon
+                    disabled
+                    small
+                  >
+                    <v-icon size="16">
+                      mdi-arrow-collapse-vertical
+                    </v-icon>
+                  </v-btn>
+                </template>
+                <span>Collapse All</span>
+              </v-tooltip>
+            </div>
+          </v-card-subtitle>
+          <v-divider />
+          <v-expansion-panels
+            v-model="panel"
+            accordion
+            flat
+            multiple
+            tile
+          >
+            <v-expansion-panel
+              v-for="(item,i) in viewPanels"
+              :key="i"
+              class="grey darken-3"
+            >
+              <v-expansion-panel-header>
+                {{ item.header }}
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                Controls here
+                <div class="d-flex panel-footer pt-3">
+                  <v-spacer />
+                  <v-btn
+                    color="primary"
+                    small
+                  >
+                    Apply
+                  </v-btn>
+                </div>
+              </v-expansion-panel-content>
+              <v-divider />
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-card>
+        <v-spacer />
+        <v-footer class="pa-0">
+          <v-btn
+            block
+            color="primary"
+            depressed
+            x-large
+            tile
+          >
+            <v-icon
+              dark
+              left
+            >
+              mdi-lightning-bolt
+            </v-icon>
+            Run Simulation
+          </v-btn>
+        </v-footer>
+      </div>
     </v-navigation-drawer>
   </v-content>
 </template>
@@ -145,5 +165,8 @@ export default {
 <style scoped>
 .toolbar-items{
   width: 90%;
+}
+.drawer-wrapper {
+  height: 100%;
 }
 </style>
