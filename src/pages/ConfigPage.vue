92dc98f5950aac680f172bed7a450f3839cb1f95
Jared Tomeck <jared.tomeck@kitware.com>
2020-11-22 13:32:37 -0500

    Add footer with run button

--- src/pages/ConfigPage.vue
+++ src/pages/ConfigPage.vue
@@ -352,7 +352,25 @@
         </v-row>
       </v-col>
       <v-col cols="7">
-        Stuff
+        <div class="viz">
+          Stuff
+        </div>
+        <v-footer
+          class="py-3"
+          color="grey darken-4"
+        >
+          <v-spacer />
+          <v-btn
+            color="primary"
+            depressed
+            large
+          >
+            Run Simulation
+            <v-icon right>
+              mdi-chevron-right
+            </v-icon>
+          </v-btn>
+        </v-footer>
       </v-col>
     </v-row>
   </v-main>
@@ -401,14 +419,18 @@ export default {
 .scrollable {
   box-shadow: inset 0 -25px 25px -25px rgba(0, 0, 0, .35);
   /* set height to 100% minus the height of the v-card-title and divider combined */
-  height: calc(100% - 65px);
+  height: calc(100% - 66px);
   overflow: hidden;
   overflow-y: auto;
+}
+.viz {
+  height: calc(100vh - 132px);
 }
 </style>
 
 <style>
 .params-panel .v-expansion-panel-content__wrap {
+  background: rgba(255, 255, 255, .12);
   padding-left: 0;
   padding-right: 0;
 }
