7b65830a0eb93ea904b388d1562c85486fb143ba
Jonathan Beezley <jonathan.beezley@kitware.com>
2021-01-04 12:57:40 -0500

    Add a login dialog

--- src/main.js
+++ src/main.js
@@ -6,15 +6,18 @@ import '@/plugins/static';
 import GirderProvider from '@/plugins/girder';
 
 import App from '@/App.vue';
+import http from '@/http';
 import vuetify from '@/plugins/vuetify';
 import router from '@/router';
 
 Vue.use(Router);
 Vue.use(VueAsyncComputed);
 
-window.nli_app = new Vue({
-  provide: GirderProvider,
-  router,
-  vuetify,
-  render: (h) => h(App),
-}).$mount('#app');
+http.fetchUser().then(() => {
+  window.nli_app = new Vue({
+    provide: GirderProvider,
+    router,
+    vuetify,
+    render: (h) => h(App),
+  }).$mount('#app');
+});
