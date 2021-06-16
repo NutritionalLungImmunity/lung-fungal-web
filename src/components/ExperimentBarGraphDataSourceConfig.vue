0a4f66f5826ceb3e6c6246451b6d7df32cce92dd
Adam Knapp <adam.knapp@instanton.org>
2021-06-15 20:17:18 -0400

    first bit of bar graphs

---
+++ src/components/ExperimentBarGraphDataSourceConfig.vue
@@ -0,0 +1,104 @@
+<template>
+  <v-container
+    class="container"
+    fluid
+  >
+    <v-row>
+      <v-col>
+        <v-select
+          v-model="selectedVariables"
+          :items="itemList"
+          :menu-props="{ maxHeight: '400' }"
+          label="Select variables to plot"
+          multiple
+          chips
+          deletable-chips
+          hint="Variables"
+          persistent-hint
+          append-outer-icon="mdi-database"
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-slider
+          v-model="timePoint"
+          :max="timeBounds[1]"
+          :min="timeBounds[0]"
+          :label="`Time`"
+          thumb-label
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        Bars vs Box and Whiskers
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        Show Outliers vs Not vs Show all
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        Group by experiment vs group by variable
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  props: {
+    variables: {
+      type: Object,
+      default: undefined,
+    },
+    timeBounds: {
+      type: Array,
+      default: undefined,
+    },
+  },
+  data() {
+    return {
+      selectedVariables: [],
+      timePoint: 0,
+    };
+  },
+  computed: {
+    itemList() {
+      if (!this.variables) {
+        return [];
+      }
+      const items = [];
+      Object.entries(this.variables).forEach(([moduleName, vars]) => {
+        if (moduleName === 'time') return; // doesn't make sense to plot time here
+
+        vars.forEach((varName) => items.push({
+          text: `${moduleName} ${varName}`,
+          value: { module: moduleName, variable: varName },
+          disabled: false, // TODO: if there is a selection, disable anything
+          // with mis-matched units
+        }));
+      });
+      return items;
+    },
+  },
+  watch: {
+    selectedVariables(newSelectedVars) {
+      this.$emit('bar-graph-variable-update', newSelectedVars);
+    },
+    timePoint(newTimePoint) {
+      this.$emit('time-point-update', newTimePoint);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+    background: #FFFFFF;
+}
+</style>
